70749e079aa7af77dd5c21c5f7c8c51841676fb4
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-08-19 15:24:08 +0800

    Big refactor : change to async sql

--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from models.database import engine, database , metadata , Base
-from api import user
-
-Base.metadata.create_all(bind=engine)
+from database.config import engine, database , Base
+from api import user , auth , myself
 
 app = FastAPI()
+app.include_router(auth.router , prefix="/api")
+app.include_router(user.router , prefix="/api")
+app.include_router(myself.router , prefix="/api")
+
 
 origins = [
     "http://localhost:5173",
@@ -28,12 +30,11 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup():
-    await database.connect()
+    # create database if not exists
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-
-app.include_router(user.router , prefix="/api")
--- server/database/config.py
+++ server/database/config.py
@@ -1,7 +1,7 @@
 import os
 
-from sqlalchemy import create_engine , MetaData
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine , AsyncSession 
 from sqlalchemy.ext.declarative import declarative_base
 from databases import Database
 from dotenv import load_dotenv
@@ -9,22 +9,14 @@ from dotenv import load_dotenv
 load_dotenv()
 # DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://hello_fastapi:hello_fastapi@localhost/hello_fastapi_dev")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://hello_fastapi:hello_fastapi@localhost/hello_fastapi_dev")
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+
 # Create engine
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 # Create session
-SessionLocal = sessionmaker(bind=engine)
-
-metadata = MetaData()
+async_session = sessionmaker(engine, expire_on_commit=False, autocommit=False,class_=AsyncSession)
 
 Base = declarative_base()
 
 database = Database(DATABASE_URL)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
