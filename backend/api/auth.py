2fb8b6f1df734901f5ed4a11211cdc1c1d7d98da
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-11-22 18:42:50 +0800

    refresh token should be store in httpOnly cookies

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timedelta
 import os
+from typing import Union, Annotated
 
 from dotenv import load_dotenv
 from jose import JWTError, jwt
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Cookie, Response
 from fastapi.security import OAuth2PasswordRequestForm
 
 from auth.action import validate_user
 from auth.utils import create_access_token, create_refresh_token
 from crud.dependencies import get_user_crud
 from crud.user import UserCRUD
-from schemas.token import Token, RefreshToken
+from schemas.token import Token
 
 
 load_dotenv()
@@ -20,6 +21,7 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login", response_model=Token)
 async def login(
+    response: Response,
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: UserCRUD = Depends(get_user_crud),
 ):
@@ -34,31 +36,45 @@ async def login(
     access_token = await create_access_token(data={"username": form_data.username})
     refresh_token = await create_refresh_token(data={"username": form_data.username})
 
-    db.update_user_login(username=form_data.username)
+    await db.update_user_login(username=form_data.username)
     expired_time = (
         int(datetime.utcnow().timestamp())
         + timedelta(
             minutes=int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
         ).seconds
     )
+
+    response.set_cookie(
+        "refresh_token",
+        refresh_token,
+        httponly=True,
+        samesite="strict",
+        secure=True,
+        expires=timedelta(int(os.environ.get("REFRESH_TOKEN_EXPIRE_MINUTES", 30))),
+    )
+
     return Token(
         access_token=access_token,
-        refresh_token=refresh_token,
         expires_in=expired_time,
         token_type="Bearer",
     )
 
 
 @router.post("/refresh", response_model=Token)
-async def refresh(refersh_data: RefreshToken, db: UserCRUD = Depends(get_user_crud)):
+async def refresh(
+    response: Response,
+    refresh_data: Annotated[Union[str, None], Cookie()] = None,
+    db: UserCRUD = Depends(get_user_crud),
+):
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        print("refresh_data", refresh_data)
         payload = jwt.decode(
-            refersh_data.refresh_token,
+            refresh_data,
             os.environ.get("REFRESH_TOKEN_SECRET"),
             algorithms=[os.environ.get("JWT_ALGORITHM", "HS256")],
         )
@@ -81,9 +97,18 @@ async def refresh(refersh_data: RefreshToken, db: UserCRUD = Depends(get_user_cr
             minutes=int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
         ).seconds
     )
+
+    response.set_cookie(
+        "refresh_token",
+        refresh_token,
+        httponly=True,
+        samesite="strict",
+        secure=True,
+        expires=timedelta(int(os.environ.get("REFRESH_TOKEN_EXPIRE_MINUTES", 30))),
+    )
+
     return Token(
         access_token=access_token,
-        refresh_token=refresh_token,
         expires_in=expired_time,
         token_type="Bearer",
     )
--- backend/schemas/token.py
+++ backend/schemas/token.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 
+# `refresh_token` should be stored in HttpOnly cookie
+
 
 class Token(BaseModel):
     access_token: str
-    refresh_token: str
     expires_in: int
     token_type: str
+    # refresh_token: str
 
 
-class RefreshToken(BaseModel):
-    refresh_token: str
+# class RefreshToken(BaseModel):
+#     refresh_token: str
