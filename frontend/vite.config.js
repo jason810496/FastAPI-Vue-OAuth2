6c7f16ec988472e12ccfab1640961757449256b5
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-02-06 23:21:39 +0800

    feat : add frontend build mode ( k8s )

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log( "BACKEND_API_URL" , env.VITE_APP_API_URL);
+  return {
+    plugins: [vue()],
+  }
 })
